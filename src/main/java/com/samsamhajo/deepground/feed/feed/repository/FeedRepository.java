696f4363cf1a710bb3deb86c265ad270c35e167c
GARDEN\wjd13 <wjd1357912@gmail.com>
2025-12-31 18:28:31 +0900

    refactor: 멤버,멤버 프로필, feed 한번에 조회할 수 있도록 사용자 정의 쿼리문 작성

--- src/main/java/com/samsamhajo/deepground/feed/feed/repository/FeedRepository.java
+++ src/main/java/com/samsamhajo/deepground/feed/feed/repository/FeedRepository.java
@@ -3,6 +3,7 @@ package com.samsamhajo.deepground.feed.feed.repository;
 import com.samsamhajo.deepground.feed.feed.entity.Feed;
 import com.samsamhajo.deepground.feed.feed.exception.FeedErrorCode;
 import com.samsamhajo.deepground.feed.feed.exception.FeedException;
+import com.samsamhajo.deepground.feed.feed.model.FetchFeedResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,8 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 @Repository
@@ -47,4 +50,13 @@ public interface FeedRepository extends JpaRepository<Feed, Long>, FeedRepositor
             "   WHERE sf.feed = f) " +
             "WHERE f.id= :feedId")
     void updateCountFeedSharedById(Long feedId);
+
+    @Query("select new com.samsamhajo.deepground.feed.feed.model.FetchFeedResponse " +
+            "(m.publicId , mp.profilePublicId , f.id, m.nickname, f.content, f.likeCount, f.commentCount," +
+            "f.sharedCount, mp.profileImage, f.createdAt)" +
+            "from Feed f " +
+            "join f.member m " +
+            "left join m.memberProfile mp " +
+            "WHERE f.id = :feedId")
+    Optional<FetchFeedResponse> getByIdWithMemberAndProfile(@Param("feedId") Long feedId);
 }
